adf53126a57185d8d9a41e5d47bd3d3c16996a89
Om Mishra <ommishra1449@gmail.com>
2025-06-12 21:10:08 +0530

    message update

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -14,11 +14,11 @@ const About = () => {
                 <img src={assets.about_img} alt="" className="w-full md:max-w-[450px]" />
                 <div className="flex flex-col justify-center gap-6 ms:w-2/4 text-gray-600">
 
-                    <p className="">Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
+                    <p className="">RoyalKing was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
                     <p className="">Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and suppliers.</p>
                     <b className="text-gray-800">Our Mission</b>
                     <p>
-                        Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.
+                        Our mission at RoyalKing is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.
                     </p>
                 </div>
             </div>
